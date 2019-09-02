9342847c337198f80cdb63cd78944e66494753e1
Ryan Lopopolo <rjl@hyperbo.la>
2019-09-02 16:30:29 -0700

    Upgrade artichoke dependency
    Pull in the fix for flaky builds from artichoke/artichoke#169.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -27,7 +27,7 @@ pub fn artichoke_web_repl_init() -> u32 {
             panic!("Could not initialize interpreter");
         }
     };
-    interp.borrow_mut().capture_output();
+    interp.0.borrow_mut().capture_output();
     interp.push_context(Context::new(REPL_FILENAME));
     let mut state = Box::new(State {
         interp,
@@ -36,7 +36,7 @@ pub fn artichoke_web_repl_init() -> u32 {
     let build = meta::build_info();
     println!("{}", build);
     state.heap.allocate(build);
-    println!("{:?}", state.interp.borrow());
+    println!("{:?}", state.interp.0.borrow());
     Box::into_raw(state) as u32
 }
 
@@ -107,7 +107,7 @@ pub fn artichoke_eval(state: u32, ptr: u32) -> u32 {
     };
     let result = format!(
         "{}{}",
-        state.interp.borrow_mut().get_and_clear_captured_output(),
+        state.interp.0.borrow_mut().get_and_clear_captured_output(),
         result
     );
     let s = state.heap.allocate(result);
